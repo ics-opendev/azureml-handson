d1b5e66bfda73a9519cd259c336d65f5b406edab
yuu-eguci <yuu.eguci@gmail.com>
2022-06-16 16:44:35 +0900

    ✨ Add sources for handson

---
+++ 06_deploy/sample/test_local_echo.py
@@ -0,0 +1,21 @@
+import requests
+import json
+from azureml.core import Workspace
+from azureml.core import LocalWebservice
+
+# ワークスペースを取得
+ws = Workspace.from_config()
+
+# 実行中のWebサービスを取得
+service = LocalWebservice(workspace=ws, name="handson-local-webservice")
+# エンドポイントを取得
+scoring_uri = service.scoring_uri
+
+headers = {"Content-Type": "application/json"}
+data = {
+    "query": "Query テスト",
+    "context": "Context テスト",
+}
+data = json.dumps(data)
+response = requests.post(uri, data=data, headers=headers)
+print(response.json())
